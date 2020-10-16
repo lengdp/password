89482239dc41301f37c8fc0b053fee4d1bc391e6
lengdp <lengdp@hotmail>
2020-10-16 16:32:24 +0800

    修复bug

--- route/list.js
+++ route/list.js
@@ -1,6 +1,4 @@
 const express=require('express');
-const url=require('url');
-const querystring=require('querystring');
 const Password = require('../model/table');
 const list=express.Router();
 
@@ -9,7 +7,6 @@ list.get('/',async (req,res)=>{
     res.render('list.art',{password:password});
 })
 list.get('/remove',async (req,res)=>{
-    const {pathname,query}=url.parse(req.url,true);
     await Password.findOneAndDelete({time:req.query.time});
     res.writeHead(301,{
         Location:'/list'
